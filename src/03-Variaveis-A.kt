5c70bbc2db08eccd5c5a3ad052cd7ef48b0d679d
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-06-13 22:47:29 -0300

    Variaveis A
    Signed-off-by: Fabricio Curvello <fabricio.desenvolvedor@gmail.com>

---
+++ src/03-Variaveis-A.kt
@@ -0,0 +1,15 @@
+package com.fabriciocurvello.helloworld
+
+// https://play.kotlinlang.org/byExample/01_introduction/03_Variables
+
+// var x val
+fun main() {
+    var a: String = "initial"  // 1
+    println(a)
+    val b: Int = 1             // 2
+    val c = 3                  // 3
+
+}
+
+// var -> variável mutável
+// val -> variável imutável. Depois que recebe o valor, este não pode ser alterado.
