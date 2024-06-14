231d13817172b76f765d22e9ee5a10e4cdd7983d
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-06-13 23:43:22 -0300

    Classes
    Signed-off-by: Fabricio Curvello <fabricio.desenvolvedor@gmail.com>

---
+++ src/05-Classes.kt
@@ -0,0 +1,19 @@
+package com.fabriciocurvello.helloworld
+
+// https://play.kotlinlang.org/byExample/01_introduction/05_Classes
+
+// Classes
+
+class Customer                                  // 1 - Classe sem atributos e métodos. Possui construtor oculto.
+
+class Contact(val id: Int, var email: String)   // 2 - Classe com construtor, e os atributos id (imutável val) e email (mutável var)
+
+fun main() {
+
+    val customer = Customer()                   // 3 - Instância (obj) da classe customer (não existe new)
+
+    val contact = Contact(1, "mary@gmail.com")  // 4 - Instância da classe contact
+
+    println(contact.id)                         // 5
+    contact.email = "jane@gmail.com"            // 6 - alterando o e-mail, uma vez que é mutável (var)
+}
