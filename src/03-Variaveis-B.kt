7385010946c8493959e34466a11eabbf6532f3d1
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-06-13 22:51:44 -0300

    Variaveis B
    Signed-off-by: Fabricio Curvello <fabricio.desenvolvedor@gmail.com>

---
+++ src/03-Variaveis-B.kt
@@ -0,0 +1,10 @@
+package com.fabriciocurvello.helloworld
+
+// https://play.kotlinlang.org/byExample/01_introduction/03_Variables
+
+//Força erro
+fun main() {
+    var e: Int  // 1
+    //e = 6
+    println(e)  // 2
+}
